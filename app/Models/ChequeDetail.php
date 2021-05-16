ed3073cda55252af8f9dbaa79404b8cad6edcc95
Ashish Shetty <30448930+Shetty073@users.noreply.github.com>
2021-05-16 22:08:58 +0530

    Update ChequeDetail.php

--- app/Models/ChequeDetail.php
+++ app/Models/ChequeDetail.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ChequeDetails extends Model
+class ChequeDetail extends Model
 {
     protected $table = 'cheque_details';
 
